3b9b1f26375f8851e9275e5076719a35ca946684
andropovdv <andropov-dv@yandex.ru>
2020-09-21 15:41:55 +0300

    Верстка страницы "Материнская плата". Переделал под свои задачи (урок 21)

--- src/componets/MBoards/MBoards.jsx
+++ src/componets/MBoards/MBoards.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
-//import s from './MBoards.module.css';
+import s from './MBoards.module.css';
 
 const MBoards = (props) => {
     return (
-        <div>Материнские платы</div>
+        <div className={s.mboards}>
+            <div className={s.mboard_items}>
+                <div><h4>Материнские платы</h4></div>
+                <div className={s.item + ' ' + s.active}>
+                    ASUS ROG STRIX B360-F GAMING
+                </div>
+                <div className={s.item}>
+                    ASUS PRIME H310M-R R2.0
+                </div>
+                <div className={s.item}>
+                    ASUS PRIME B365M-A
+                </div>
+                <div className={s.item}>
+                    ASUS PRIME B360M-K
+                </div>
+                <div className={s.item}>
+                    ASUS TUF B450-PRO GAMING
+                </div>
+                <div className={s.item}>
+                    ASUS TUF B450M-PRO GAMING
+                </div>
+            </div>
+            <div className={s.mb_adds}>
+                <div className={s.item}>
+                    <b>Добавить</b>
+                </div>
+                <div className={s.item}>
+                    <textarea></textarea>
+                </div>
+                <div className={s.item}>
+                    <button>Добавить</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
