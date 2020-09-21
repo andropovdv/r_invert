b792ed306b7411bf3a0082b718c21a25a284939a
andropovdv <andropov-dv@yandex.ru>
2020-09-21 17:04:34 +0300

    Сделал активными списки Mb и Cpu (route exaсt попробывал) -- урок 22

--- src/componets/Cpus/Cpus.jsx
+++ src/componets/Cpus/Cpus.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import s from './Cpus.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Cpus = (props) => {
     return (
         <div className={s.cpus}>
-            <div className={s.cpu_item}>
+            <div className={s.cpu_items}>
                 <div><h4>Процесоры</h4></div>
-                <div className={s.item + ' ' + s.active}>
-                    AMD Ryzen 5 2600
+                <div className={s.item}>
+                    <NavLink to='/cpus/1' activeClassName='s.activeLink'>AMD Ryzen 5 2600</NavLink>
                 </div>
                 <div className={s.item}>
-                    INTEL Core i5 9400F
+                    <NavLink to='/cpus/2' activeClassName='s.actveLink'>INTEL Core i5 9400F</NavLink>
                 </div>
                 <div className={s.item}>
-                    AMD Ryzen 7 2700
+                    <NavLink to='/cpus/2' activeClassName='s.actveLink'>AMD Ryzen 7 2700</NavLink>
                 </div>
                 <div className={s.item}>
-                    AMD Ryzen 5 3600
+                    <NavLink to='/cpus/2' activeClassName='s.actveLink'>AMD Ryzen 5 3600</NavLink>
                 </div>
                 <div className={s.item}>
-                    AMAMD A8 9600
+                    <NavLink to='/cpus/2' activeClassName='s.actveLink'>AMD A8 9600</NavLink>
                 </div>
                 <div className={s.item}>
-                    INTEL Core i3 9100F
+                    <NavLink to='/cpus/2' activeClassName='s.actveLink'>INTEL Core i3 9100F</NavLink>
                 </div>
             </div>
             <div className={s.cpu_add}>
@@ -38,7 +39,7 @@ const Cpus = (props) => {
                 <div className={s.item}>
                     <input></input>
                 </div>
-                
+
             </div>
         </div>
     )
--- src/componets/MBoards/MBoards.jsx
+++ src/componets/MBoards/MBoards.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import s from './MBoards.module.css';
+import { NavLink } from 'react-router-dom';
 
 const MBoards = (props) => {
     return (
         <div className={s.mboards}>
             <div className={s.mboard_items}>
                 <div><h4>Материнские платы</h4></div>
-                <div className={s.item + ' ' + s.active}>
-                    ASUS ROG STRIX B360-F GAMING
+                <div className={s.item}>
+                    <NavLink to='/Mboards/1' activeClassName={s.activeLink}>ASUS ROG STRIX B360-F GAMING</NavLink>
                 </div>
                 <div className={s.item}>
-                    ASUS PRIME H310M-R R2.0
+                    <NavLink to='/Mboards/2' activeClassName={s.activeLink}>ASUS PRIME H310M-R R2.0</NavLink>
                 </div>
                 <div className={s.item}>
-                    ASUS PRIME B365M-A
+                    <NavLink to='/Mboards/3' activeClassName={s.activeLink}>ASUS PRIME B365M-A</NavLink>
                 </div>
                 <div className={s.item}>
-                    ASUS PRIME B360M-K
+                    <NavLink to='/Mboards/4' activeClassName={s.activeLink}>ASUS PRIME B360M-K</NavLink>
                 </div>
                 <div className={s.item}>
-                    ASUS TUF B450-PRO GAMING
+                    <NavLink to='/Mboards/5' activeClassName={s.activeLink}>ASUS TUF B450-PRO GAMING</NavLink>
                 </div>
                 <div className={s.item}>
-                    ASUS TUF B450M-PRO GAMING
+                    <NavLink to='/Mboards/6' activeClassName={s.activeLink}>ASUS TUF B450M-PRO GAMING</NavLink>
                 </div>
             </div>
             <div className={s.mb_adds}>
